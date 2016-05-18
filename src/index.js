12cb19c07ca9c8cfcc225ab33bf1ec28735c0b16
kkaabbaa <nasnetstep@gmail.com>
2016-05-18 19:20:32 +0300

    Start redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import 'babel-polyfill'
 import React from 'react'
-import { render } from 'react-dom'
+import {render} from 'react-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import App from './containers/App'
 
+const store = createStore(() => {
+}, {})
 
 render(
-<App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById('root')
 )
